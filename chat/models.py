778c9115882bdc21ebfc16e6ae878e9c01ec077e
vansh1999 <vansh.bhardwaj1999@gmail.com>
2019-11-11 13:27:44 +0530

    chat app

---
+++ chat/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class Room(models.Model):
+    """Represents chat rooms that users can join"""
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=100)
+    slug = models.CharField(max_length=50)
+
+    def __str__(self):
+        """Returns human-readable representation of the model instance."""
+        return self.name
+
+
